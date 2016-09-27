5882b93dfdc466e80801733392f1b713da89ce0c
Sara <dibartolomeo.sara@gmail.com>
2016-09-28 01:31:32 +0200

    tmux

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,13 @@
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+alias "cd.."="cd ../"
+
+alias zshconfig="mate ~/.zshrc"
+alias up="cd ../"
+alias fuck='sudo $(fc -ln -1)'
