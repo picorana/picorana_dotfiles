4243e54f342f8fd5b8fbb63bc330798fd6ae911a
picorana <dibartolomeo.sara@gmail.com>
2017-05-09 09:39:36 +0200

    aa

--- zsh/.zsh/stuff_to_organize.zsh
+++ zsh/.zsh/stuff_to_organize.zsh
@@ -2,20 +2,20 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH=/home/sara/.oh-my-zsh
+  export ZSH=/home/rana/.oh-my-zsh
 
 #case $- in *i*)
 #    [ -z "$TMUX" ] && exec tmux
 #esac
 
-if [ "$TMUX" = "" ]; then 
-    TERM=xterm-256color tmux 
-fi
+#if [ "$TMUX" = "" ]; then 
+#    TERM=xterm-256color tmux 
+#fi
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="geometry"
+ZSH_THEME="spaceship"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
