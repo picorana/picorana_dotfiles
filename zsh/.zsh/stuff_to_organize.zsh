6cfd3c7bd168b318180a0e3ffc62c40216110c6e
picorana <dibartolomeo.sara@gmail.com>
2017-10-08 08:57:25 +0200

    switching to i3

--- zsh/.zsh/stuff_to_organize.zsh
+++ zsh/.zsh/stuff_to_organize.zsh
@@ -22,7 +22,7 @@ source /usr/local/bin/virtualenvwrapper.sh
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-#ZSH_THEME="geometry"
+ZSH_THEME="avit"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
